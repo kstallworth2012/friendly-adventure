9aecdc7c42185e7b80c96dac672f3db1491e27a3
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-08-19 19:00:41 -0400

    Initial Update

---
+++ src/main/java/com/cash/flow/CashFlowManager/CashFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cash.flow.CashFlowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cash/flow/CashFlowManager/CashFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cash.flow.CashFlowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
